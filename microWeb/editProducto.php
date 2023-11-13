7f3c1a1cdbf8ec7e65181786dd7ac45916c3769b
camilodelgado23 <126426982+camilodelgado23@users.noreply.github.com>
2023-11-12 19:15:35 -0500

    Update editProducto.php

--- microWeb/editProducto.php
+++ microWeb/editProducto.php
@@ -1,17 +1,19 @@
 <?php
+session_start(); // Inicia la sesión al principio del código
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Verificar si se recibieron los datos necesarios
     if (isset($_POST["productId"]) && isset($_POST["newStock"])) {
         // Obtener el ID del producto y el nuevo inventario desde el formulario
         $productId = $_POST["productId"];
         $newStock = $_POST["newStock"];
-        
+
         // Construir el JSON con el nuevo inventario
         $data = json_encode(array("inventario" => $newStock));
-        
+
         // URL de la API para actualizar el producto
         $url = "http://microProductos:3002/productos/" . $productId;
-        
+
         // Inicializar cURL para enviar la solicitud PUT
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
@@ -21,26 +23,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             "Content-Length: " . strlen($data)
         ));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        
+
         // Realizar la solicitud PUT
         $response = curl_exec($ch);
-        
-        if ($response === false) {
-            echo "Error en la conexión: " . curl_error($ch);
-            header("Location:index.html");
-            exit;
-        } else {
-            // La solicitud fue exitosa, puedes procesar la respuesta si es necesario
-            echo "Inventario actualizado exitosamente.";
-            session_start();
-            $_SESSION["mensaje_exito"] = "El inventario ha sido actualizado. ✅";
-            header("Location:admin-prod.php");
-            exit;
 
-        }
-        
         // Cerrar la conexión cURL
         curl_close($ch);
+
+        if ($response === false) {
+            echo "Error en la conexión: " . curl_error($ch);
+            exit; // Salir sin redirección en caso de error
+        } else {
+            // La solicitud fue exitosa, puedes procesar la respuesta si es necesario
+            $_SESSION["mensaje_exito"] = "Inventario actualizado exitosamente. ✅ ";
+            header("Location: admin-prod.php");
+            exit;
+        }
     } else {
         echo "Faltan datos necesarios para la actualización.";
     }
